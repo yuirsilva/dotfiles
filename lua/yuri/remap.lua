5d21203ed09d47fb508d2b8c0d4d9cc8cfb37633
Yuri Silva <yrgamerbr@gmail.com>
2023-02-20 18:06:04 -0300

    command...

--- lua/yuri/remap.lua
+++ lua/yuri/remap.lua
@@ -5,6 +5,12 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Select all
+vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- New tab
+vim.keymap.set('n', 'te', ':tabedit')
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
